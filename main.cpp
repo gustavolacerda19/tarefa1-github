0238665389c7f8bdeb36af6667710cdc4f5216f9
Gustavo Lacerda <gustavolacerda655@gmail.com>
2020-05-06 13:22:41 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string sexo;
+    int cerveja, refrigerante, espetinho;
+
+    cout << "Sexo: ";
+    cin >> sexo;
+
+    cout << "Quantidade de cervejas: ";
+    cin >> cerveja;
+
+    cout << "Quantidade de refrigerantes: ";
+    cin >> refrigerante;
+
+    cout << "Quantidade de espetinhos: ";
+    cin >> espetinho;
+
     return 0;
 }
